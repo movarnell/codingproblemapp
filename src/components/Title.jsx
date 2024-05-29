eb3789c9d63e2e14f33515c4ac60307aca6d0190
movarnell <movarnell@gmail.com>
2024-05-29 15:54:43 -0500

    chore: Update Title component with beta version and additional content

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -1,7 +1,23 @@
 function Title() {
-  return <div>
-    <h1 className="text-4xl text-center">Code Problems for You</h1>
+  return (
+    <div>
+      <h1 className="text-4xl text-center">Code Problems for You - Beta</h1>
 
-  </div>;
+      <p className="text-xs text-center text-red-500 font-bold italic">
+        Some down time may occur...Improving backend.
+        <br />
+        Apologies if you encounter any issues. This is a free service.
+      </p>
+      <div className="flex justify-center my-5">
+        <a href="https://www.buymeacoffee.com/michaelvarnell" target="_blank">
+          <img
+            src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+            alt="Buy Me A Coffee"
+            width="150px"
+          />
+        </a>
+      </div>
+    </div>
+  );
 }
 export default Title;
